25fa87e8366c60e7627aa1fc07aa3cf0232c3d4f
vikram u k <vikramuk@gmail.com>
2021-12-16 12:35:41 +0530

    Create Run.bat

---
+++ Run.bat
@@ -0,0 +1,4 @@
+cd C:\Users\vikram.uk old\Documents\Data\Alcon\AlconProject\Tests\Sprint81\Formal\Hotfix1\Karate
+C:\Progapps\apache-maven-3.6.3\bin\mvn test -Dkarate.options="classpath:11BoxFormula/Test_CalculateFormula.feature"
+"C:\Users\vikram.uk old\Documents\Data\Alcon\AlconProject\Tests\Sprint81\Formal\Hotfix1\Karate\target\surefire-reports\RenameReport.bat"
+
